b37948b5fe14474c2412554894cfdb7ea271d94c
Usernamegrv <139330806+Usernamegrv@users.noreply.github.com>
2024-03-15 11:07:14 +0100

    modal edit UserName

--- Frontend/argentBank/src/components/modal/Modal.jsx
+++ Frontend/argentBank/src/components/modal/Modal.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setUserName } from "../../reducers/userSlice.js";
 
-const Modal = () => {
+import "./Modal.css";
+
+import PropTypes from "prop-types";
+
+const Modal = ({ closeModal }) => {
   const [newUserName, setNewUserName] = useState("");
   const dispatch = useDispatch();
 
@@ -14,19 +18,37 @@ const Modal = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(setUserName(newUserName));
+    closeModal();
   };
+
+  const currentUser = useSelector(
+    (state) => state.userReducer.currentUser.userName
+  );
+  console.log(currentUser);
 
   return (
     <div className="modal">
-      <input
-        type="text"
-        value={newUserName}
-        onChange={handleInputChange}
-        placeholder="Enter new username"
-      />
-      <button onClick={handleSubmit}>Submit</button>
+      <div className="modal-content">
+        <h2>Edit user info</h2>
+        <input
+          type="text"
+          onChange={handleInputChange}
+          placeholder={currentUser}
+        />
+        <input type="text" placeholder="First name" readOnly disabled />
+        <input type="text" placeholder="Last name" readOnly disabled />
+      </div>
+
+      <div>
+        <button onClick={handleSubmit}>Save</button>
+        <button onClick={closeModal}>Cancel</button>
+      </div>
     </div>
   );
+};
+
+Modal.propTypes = {
+  closeModal: PropTypes.func.isRequired,
 };
 
 export default Modal;
